fabb58032bb5b95847a7e3092dd60d5da45ce29b
talbx <dev@talbx.de>
2023-07-21 15:10:22 +0200

    chore: cleanup

--- pkg/service/notify_bridge.go
+++ pkg/service/notify_bridge.go
@@ -14,7 +14,7 @@ var NotifyCache = make(map[string]time.Time, 0)
 
 func (bridge NotifyBridge) Notify(m model.TuyaHumidity, n NotificationType) {
 	if n == RESOLVED {
-		bridge.RealNotifier.Notify(m,n)
+		bridge.RealNotifier.Notify(m, n)
 		return
 	}
 
@@ -25,21 +25,10 @@ func (bridge NotifyBridge) Notify(m model.TuyaHumidity, n NotificationType) {
 		if diff.Minutes() >= 30 {
 			NotifyCache[m.Device] = now
 			model.SugaredLogger.Infof("The NotifyBridge found out that the last notification for firing device %v is overdue by %v (>= 30 Minutes)", m.Device, diff.Minutes())
-			bridge.RealNotifier.Notify(m,n)
+			bridge.RealNotifier.Notify(m, n)
 			return
 		}
 		model.SugaredLogger.Infof("The NotifyBridge found out that the last notification for firing device %v is only %v minutes old. Therefore the notifier will not be triggered", m.Device, diff.Minutes())
 	}
 
 }
-
-
-// wenn ein update für schlafzimmer reinkommt
-	// ziehe letzten wert für schlafzimmer
-		// wenn letzter wert == OK und neuer wert != OK -> notification
-		// wenn letzter wert == OK und neuer wert == OK -> tue nichts
-		// wenn letzter wert != OK und neuer wert == OK -> notification
-		// wenn letzter wert != OK und neuer wert != OK 
-			// schaue letzten send timestamp an
-			// wenn letzter timestamp > 30 min her -> notification
-			// wenn letzter timestamp < 30min her -> tue nichts
